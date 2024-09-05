9fdc35d72bbcd8f8ce2b1bf28155fc30dda1d7bf
syclonce <syclonce@gmail.com>
2024-09-05 08:32:19 +0000

    clear

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -222,14 +222,6 @@
                   </li>
                 </ul>
             </li>
-                  <li class="nav-item">
-                    <a href="{{ route('finance.daig') }}" class="nav-link {{ \Route::is('finance.daig') ? 'active' : '' }}">
-                      <i class="fas fa-fw fa-solid fa-square-poll-horizontal" style="color: #63E6BE;"></i>
-                      <p>Prosedur Pembayaran</p>
-                    </a>
-                  </li>
-                </ul>
-            </li>
 
             <li class="nav-item">
                 <a href="{{ route('setweb') }}"
