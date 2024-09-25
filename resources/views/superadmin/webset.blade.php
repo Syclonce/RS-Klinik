bad65dbb01d79b9cba586ad29d8d98188e8fe825
Syclonce <Syclonce@gmail.com>
2024-09-25 15:35:15 +0700

    update logo

--- resources/views/superadmin/webset.blade.php
+++ resources/views/superadmin/webset.blade.php
@@ -18,37 +18,50 @@
         <!-- Main content -->
         <section class="content">
             <div class="container-fluid">
-                <!-- Main row -->
+                {{-- <!-- Main row -->
                 <div class="row">
-                    <div class="col-md-3">
 
+                    <!-- /.col -->
+                </div> --}}
+
+                <div class="row">
+                    <!-- Kolom kiri (Profile Image) -->
+                    <div class="col-md-3">
+                        <style>
+                            .profile-user-img {
+                                width: 100px; /* Ukuran lebar gambar, ubah sesuai kebutuhan */
+                                height: 100px; /* Ukuran tinggi gambar, ubah sesuai kebutuhan */
+                                border-radius: 50%; /* Membuat gambar berbentuk bulat */
+                                border: 2px solid #ddd; /* Menambahkan border pada gambar */
+                            }
+                        </style>
                         <!-- Profile Image -->
                         <div class="card card-primary card-outline">
                             <div class="card-body box-profile">
                                 @foreach ($webset as $webset)
-                                    <br>
-                                    <div class="text-center">
-                                        <img class="profile-user-img img-fluid img-circle"
-                                            src="{{ asset('webset/' . $webset->logo_app) }}" alt="User profile picture">
-                                    </div>
-                                    <br>
-                                    <br>
-                                    <ul class="list-group list-group-unbordered mb-3">
-                                        <li class="list-group-item text-center">
-                                            <b>Name web : </b> <a class="float">{{ $webset->name_app }}</a>
-                                        </li>
-                                    </ul>
+                                <div class="text-center">
+                                    <img class="profile-user-img img-circle"
+                                        src="{{ asset('webset/' . $webset->logo_app) }}" alt="User profile picture">
+                                </div>
+
+
+                                <br>
+                                <ul class="list-group list-group-unbordered mb-3">
+                                    <li class="list-group-item text-center">
+                                        <b>Name web : </b> <a class="float">{{ $webset->name_app }}</a>
+                                    </li>
+                                </ul>
+                                <br>
+                                @endforeach
                             </div>
                             <!-- /.card-body -->
-                            @endforeach
                         </div>
                         <!-- /.card -->
                     </div>
 
-
-                    <!-- /.col -->
+                    <!-- Kolom kanan (Form untuk mengupdate Name App dan Logo) -->
                     <div class="col-md-9">
-                        <div class="card">
+                        <div class="card card-primary card-outline">
                             <form action="{{ route('setweb.update') }}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 <div class="card-body">
@@ -61,10 +74,8 @@
                                         <label for="logoApp">File Logo APP</label>
                                         <div class="input-group">
                                             <div class="custom-file">
-                                                <input type="file" name="logo_app" class="custom-file-input"
-                                                    id="logoApp">
-                                                <label class="custom-file-label"
-                                                    for="logoApp">{{ $webset->logo_app }}</label>
+                                                <input type="file" name="logo_app" class="custom-file-input" id="logoApp">
+                                                <label class="custom-file-label" for="logoApp">{{ $webset->logo_app }}</label>
                                             </div>
                                         </div>
                                     </div>
@@ -78,12 +89,76 @@
                                     <button type="submit" class="btn btn-primary">Submit</button>
                                 </div>
                             </form>
-
                         </div>
                         <!-- /.card -->
                     </div>
-                    <!-- /.col -->
+
+                    <div class="col-md-6">
+                        <div class="card card-primary card-outline">
+                            <form action="{{ route('setweb.update') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="nameApp">Name APP</label>
+                                        <input type="text" name="name_app" class="form-control" id="nameApp"
+                                            value="{{ $webset->name_app }}" placeholder="Enter app name" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="logoApp">File Logo APP</label>
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input type="file" name="logo_app" class="custom-file-input" id="logoApp">
+                                                <label class="custom-file-label" for="logoApp">{{ $webset->logo_app }}</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="hidden" name="id" value="{{ $webset->id }}">
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card card-primary card-outline">
+                            <form action="{{ route('setweb.update') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="nameApp">Name APP</label>
+                                        <input type="text" name="name_app" class="form-control" id="nameApp"
+                                            value="{{ $webset->name_app }}" placeholder="Enter app name" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="logoApp">File Logo APP</label>
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input type="file" name="logo_app" class="custom-file-input" id="logoApp">
+                                                <label class="custom-file-label" for="logoApp">{{ $webset->logo_app }}</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="hidden" name="id" value="{{ $webset->id }}">
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.card -->
+                    </div>
                 </div>
+
                 <!-- /.row (main row) -->
             </div><!-- /.container-fluid -->
         </section>
